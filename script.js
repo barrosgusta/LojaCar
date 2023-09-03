82c5621b0f3d4f9edfdf1abd59fbe53cbc982439
Gustavo Verdi <gustavoverdi2015@gmail.com>
2023-09-03 12:56:25 -0300

    update anuncio

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-    $(function() {
-    $(".toggle").on("click", function() {
-        if ($(".item").hasClass("active")) {
-            $(".item").removeClass("active");
-        } else {
-            $(".item").addClass("active");
-        }
+
+    // Script Marca do Veículo
+    var select = document.getElementById("car_brand");
+
+    // Ordena Ordem Alfabetica
+    var options = Array.from(select.options);
+    options.sort(function(a, b) {
+        return a.text.localeCompare(b.text);
     });
-});
+
+    // Clear the select element
+    select.innerHTML = "";
+
+    // Add the sorted options back to the select element
+    options.forEach(function(option) {
+        select.appendChild(option);
+    });
+    // Obtenha referências para os elementos de seleção
